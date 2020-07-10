e2f381c984711ee8ccc22332afc1a758db5d9095
Stanley Yip <stanley@khanacademy.org>
2020-07-10 13:37:55 -0700

    autobuild?

--- src/pr-notify-push.js
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env node
-require('@babel/register');var core=require('@actions/core');var _require=require('./main.js'),runPush=_require.runPush;try{runPush();}catch(error){core.setFailed(error.message);}
--- src/pr-notify.js
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env node
-require('@babel/register');var core=require('@actions/core');var _require=require('./main.js'),runPullRequest=_require.runPullRequest;try{runPullRequest();}catch(error){core.setFailed(error.message);}
