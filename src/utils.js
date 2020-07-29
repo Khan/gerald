5b92f1866edd071ca105041d6790d765460d81c0
Stanley Yip <stanley@khanacademy.org>
2020-07-29 11:53:17 -0700

    better style for getting geraldignore files/directories

--- src/utils.js
+++ src/utils.js
@@ -5,14 +5,28 @@ import fs from 'fs';
 import fg from 'fast-glob'; // flow-uncovered-line
 
 import {execCmd} from './execCmd';
-const geraldIgnore =
-    fs.readFileSync('.geraldignore', 'utf-8') || fs.readFileSync('.gitignore', 'utf-8');
-const globOptions = {
-    dot: true,
-    ignore: geraldIgnore.split('\n'),
-};
 
 type NameToFiles = {[name: string]: string[], ...};
+
+/**
+ * @desc Read ./.geraldignore if it exists. Otherwise, read ./.gitignore.
+ * Split the file by newlines to serve as the list of files/directories to
+ * ignore for Gerald.
+ */
+const getGeraldIgnore = (): Array<string> => {
+    if (fs.existsSync('.geraldignore')) {
+        return fs.readFileSync('.geraldignore', 'utf-8').split('\n');
+    } else if (fs.existsSync('.gitignore')) {
+        return fs.readFileSync('.gitignore', 'utf-8').split('\n');
+    }
+    return [];
+};
+
+const geraldIgnore = getGeraldIgnore();
+const globOptions = {
+    dot: true,
+    ignore: geraldIgnore,
+};
 
 /**
  * @desc Add the username/files pair to the nameToFilesObj if the
