90862045cc884cdee7b91656711deca6f6d4d6a0
Stanley Yip <stanley@khanacademy.org>
2020-07-31 11:29:25 -0700

    oh

--- getFetchDepth.js
+++ getFetchDepth.js
@@ -1,29 +1,9 @@
 #!/usr/bin/env node
-// @flow
-
-import {
-    type Octokit,
-    type Octokit$IssuesListCommentsResponseItem,
-    type Octokit$PullsListResponseItem,
-    type Octokit$PullsListCommitsResponseItemCommit,
-} from '@octokit/rest';
-
-type Context = {|
-    issue: {|owner: string, repo: string, number: number|},
-    payload: {|
-        pull_request: Octokit$PullsListResponseItem,
-        before: string,
-        after: string,
-        commits: {id: string, ...Octokit$PullsListCommitsResponseItemCommit}[],
-    |},
-    actor: string,
-|};
-require('@babel/register');
 
 const core = require('@actions/core'); //flow-uncovered-line
 const octokit = require('@actions/github'); //flow-uncovered-line
 
-const extraPermGithub: Octokit = new octokit.GitHub(process.argv[2]);
-const context: Context = octokit.context;
+const extraPermGithub = new octokit.GitHub(process.argv[2]);
+const context = octokit.context;
 
 core.setOutput('fetchDepth', context.payload.commits.length); //flow-uncovered-line
