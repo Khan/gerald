f3c4e7dce8520458e577ac9bd609e6834b2225dc
Stanley Yip <33562077+yipstanley@users.noreply.github.com>
2020-08-04 15:40:23 -0700

    Fixed bug where fast-glob was crashing in Mobile repo (#20)
    ## Summary:
    
    Same fix as in Khan/our-lovely-cli/pull/117
    
    Issue: WEB-2657
    
    ## Test plan:
    See Khan/our-lovely-cli/pull/117
    
    Author: yipstanley
    
    Reviewers: kevinbarabash, yipstanley
    
    Required Reviewers: 
    
    Approved by: kevinbarabash
    
    Checks: ✅ Flow Coverage, ✅ Jest Coverage, ✅ Jest, ✅ Eslint, ✅ autofix, ✅ lint_and_unit, ✅ build_index
    
    Pull request URL: https://github.com/Khan/gerald/pull/20

--- src/__test__/utils.test.js
+++ src/__test__/utils.test.js
@@ -5,6 +5,7 @@ import {
     __turnPatternIntoRegex,
     __parseUsername,
     __pushOrSetToBin,
+    __filterIgnoreFiles,
     getNotified,
     getReviewers,
     parseExistingComments,
@@ -324,5 +325,66 @@ describe('test get file diffs', () => {
         expect(result['testFile']).toEqual(mockTestFileDiff);
 
         expect(result['otherFile.js']).toEqual(mockOtherFileDiff);
+    });
+});
+
+describe('test that ignore files are parsed correctly', () => {
+    it('should work', () => {
+        const testIgnoreFile = `src
+.bashrc
+.github`;
+
+        const ignoredFiles = __filterIgnoreFiles(testIgnoreFile);
+
+        expect(ignoredFiles.length).toEqual(3);
+        expect(ignoredFiles).toEqual(['src', '.bashrc', '.github']);
+    });
+
+    it('should ignore new lines', () => {
+        const testIgnoreFile = `src
+
+.bashrc
+
+.github`;
+
+        const ignoredFiles = __filterIgnoreFiles(testIgnoreFile);
+
+        expect(ignoredFiles.length).toEqual(3);
+        expect(ignoredFiles).toEqual(['src', '.bashrc', '.github']);
+    });
+
+    it('should ignore comments', () => {
+        const testIgnoreFile = `src
+.bashrc
+.github
+# don't read this line!`;
+
+        const ignoredFiles = __filterIgnoreFiles(testIgnoreFile);
+
+        expect(ignoredFiles.length).toEqual(3);
+        expect(ignoredFiles).toEqual(['src', '.bashrc', '.github']);
+    });
+
+    it('should ignore the comment part of a line, but not before the comment', () => {
+        const testIgnoreFile = `src
+.bashrc
+.github # read .github, but not this part of it!
+# don't read this line!`;
+
+        const ignoredFiles = __filterIgnoreFiles(testIgnoreFile);
+
+        expect(ignoredFiles.length).toEqual(3);
+        expect(ignoredFiles).toEqual(['src', '.bashrc', '.github']);
+    });
+
+    it('should ignore weird spacing problems', () => {
+        const testIgnoreFile = `src
+    .bashrc
+.github    `;
+
+        const ignoredFiles = __filterIgnoreFiles(testIgnoreFile);
+
+        expect(ignoredFiles.length).toEqual(3);
+        expect(ignoredFiles).toEqual(['src', '.bashrc', '.github']);
     });
 });
