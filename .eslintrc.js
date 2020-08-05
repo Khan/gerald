8fb9af6bb926093dfb6defb02b4d0527849f1191
Stanley Yip <stanley@khanacademy.org>
2020-08-05 08:25:30 -0700

    eslint merge

--- .eslintrc.js
+++ .eslintrc.js
@@ -101,11 +101,7 @@ module.exports = {
     },
     overrides: [
         {
-<<<<<<< HEAD
             files: ['src/core.js', 'src/main.js', 'src/execCmd.js'],
-=======
-            files: ['src/pr-notify.js', 'src/main.js', 'src/execCmd.js'],
->>>>>>> Automated build push
             rules: {
                 'import/no-commonjs': 'off',
             },
