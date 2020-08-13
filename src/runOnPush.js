7c360f3a84459692ff910248aeacdc761091eafc
Stanley Yip <stanley@khanacademy.org>
2020-08-13 11:08:43 -0700

    comments

--- src/runOnPush.js
+++ src/runOnPush.js
@@ -63,6 +63,9 @@ export const runPush = async (usedContext: Context) => {
                     const committedAndSquashedDiff = diffByLines.filter(line =>
                         squashedDiffByLines.includes(line),
                     );
+                    console.log(diffByLines);
+                    console.log(squashedDiffByLines);
+                    console.log(committedAndSquashedDiff);
                     fileDiffs[file] = committedAndSquashedDiff.join('\n');
                 }
             }
