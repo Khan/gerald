852537bf3d30f07403e7695b35231e942940faa9
Stanley Yip <33562077+yipstanley@users.noreply.github.com>
2020-08-14 16:39:56 -0700

    Make requests only to people/teams that have not reviewed (#53)
    ## Summary:
    
    Previously, if Gerald requested a review from Khan/team1 and owner1, and owner1 and a member of Khan/team1 reviewed the pull request, Gerald would rerequest reviews from KHan/team1 and owner1 if the pull request was updated. This PR changes Gerald so that it doesn't request reviews from people or teams that have already reviewed the pull request.
    
    There was also a bug here where the change to `@octokit/rest_vx.x.x.js` was being interpreted as a team and failing on `git land`. This also fixes that by escaping all `@` symbols with `%40`.
    
    Issue: WEB-2795
    
    ## Test plan:
    https://github.com/Khan/gerald/pull/52
    
    Author: yipstanley
    
    Reviewers: lillialexis, #frontend-infra
    
    Required Reviewers: 
    
    Approved by: lillialexis
    
    Checks: ✅ Flow Coverage, ✅ Jest Coverage, ✅ Jest, ✅ Eslint, ✅ gerald, ✅ build_index, ✅ lint_and_unit, ✅ autofix
    
    Pull request URL: https://github.com/Khan/gerald/pull/53

--- src/runOnPullRequest.js
+++ src/runOnPullRequest.js
@@ -64,6 +64,42 @@ const updatePullRequestComment = async (
     }
 };
 
+const makeReviewRequests = async (reviewers: Array<string>, teamReviewers: Array<string>) => {
+    // figure out who has already reviewed
+    const {data: reviews} = await extraPermGithub.pulls.listReviews({
+        ...ownerAndRepo,
+        pull_number: context.issue.number,
+    });
+    const alreadyReviewed: Array<string> = reviews.map(review => review.user.login);
+
+    // unfulfilled reviewers = everyone who hasn't reviewed
+    const unfulfilledReviewers = reviewers.filter(reviewer => !alreadyReviewed.includes(reviewer));
+    const unfulfilledTeams = teamReviewers;
+
+    for (const team of teamReviewers) {
+        const {data: membership} = await extraPermGithub.teams.listMembersInOrg({
+            org: ownerAndRepo.owner,
+            team_slug: team,
+        });
+        const members = membership.map(member => member.login);
+
+        // if a requested team has a review from a member, consider it fulfilled
+        for (const reviewer of alreadyReviewed) {
+            if (members.includes(reviewer)) {
+                unfulfilledTeams.splice(unfulfilledTeams.indexOf(team), 1);
+                break;
+            }
+        }
+    }
+
+    await extraPermGithub.pulls.createReviewRequest({
+        ...ownerAndRepo,
+        pull_number: context.issue.number,
+        reviewers: unfulfilledReviewers,
+        team_reviewers: unfulfilledTeams,
+    });
+};
+
 export const runOnPullRequest = async () => {
     // get the files changed between the head of this branch and the origin of the base branch
     const filesChanged = (
@@ -109,12 +145,7 @@ export const runOnPullRequest = async () => {
         {...ownerAndRepo, pull_number: context.issue.number},
         extraPermGithub,
     );
-    await extraPermGithub.pulls.createReviewRequest({
-        ...ownerAndRepo,
-        pull_number: context.issue.number,
-        reviewers: actualReviewers,
-        team_reviewers: teamReviewers,
-    });
+    await makeReviewRequests(actualReviewers, teamReviewers);
 
     await updatePullRequestComment(megaComment, notified, reviewers, requiredReviewers);
 };
