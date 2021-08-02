deb1f2fe4deede590662c3caa1c1c34eeb70d70a
Lilli Szafranski <lilli@khanacademy.org>
2021-08-02 11:25:00 -0700

    trying again

--- src/runOnPullRequest.js
+++ src/runOnPullRequest.js
@@ -101,17 +101,18 @@ const makeReviewRequests = async (reviewers: Array<string>, teamReviewers: Array
 };
 
 export const runOnPullRequest = async () => {
+    const asymmetricBaseStr = `origin/${context.payload.pull_request.base.ref}...`;
     // get the files changed between the head of this branch and the origin of the base branch
     const filesChanged = (
         await execCmd('git', [
             'diff',
-            `origin/${context.payload.pull_request.base.ref}...`,
+            asymmetricBaseStr,
             '--name-only',
         ])
     ).split('\n');
-    // get the actual diff between the head of this branch adn the origin of the base branch, split by files
-    const fileDiffs = await getFileDiffs('origin/' + context.payload.pull_request.base.ref);
-    const fileContents = await getFileContents('origin/' + context.payload.pull_request.base.ref);
+    // get the actual diff between the head of this branch and the origin of the base branch, split by files
+    const fileDiffs = await getFileDiffs(asymmetricBaseStr);
+    const fileContents = await getFileContents(asymmetricBaseStr);
 
     // figure out who to notify and request reviews from
     const notified = getNotified(
